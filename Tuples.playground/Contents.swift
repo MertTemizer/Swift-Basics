85837846e7ec0de176e4e5ab5867cd61402c5048
Mert Temizer <mert10temizer@gmail.com>
2024-10-13 23:37:07 +0300

    Add files via upload

---
+++ Tuples.playground/Contents.swift
@@ -0,0 +1,14 @@
+var vegetable = ("Carrot", 3, true)
+
+var (vegType, quantity, inStock) = vegetable
+
+print(quantity)
+
+var veg = (vegType: "Carrot", quantity: 3, inStock: true)
+
+print(veg.vegType)
+
+print(veg.2)
+
+
+
